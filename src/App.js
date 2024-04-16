11983745a6f94901f1f73516abcfdc75bc874668
beyzagoknl <beyzagoknl@gmail.com>
2024-04-16 17:04:13 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { createUseStyles } from "react-jss";
 import { useWebcamCapture } from "./useWebcamCapture";
+import Navbar from "./components/Navbar/Navbar";
 // import logo from './logo.svg'
 import logo from "./slap.png";
 
@@ -107,22 +108,13 @@ function App(props) {
 
   return (
     <div className={classes.App}>
+      <Navbar />
       <header className={classes.Header}>
         <h1>SlapSticker</h1>
         <p>
           Have you ever said something so dumb, you just wanted to slap
           yourself? Well now you can!
         </p>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">home</Link>
-            </li>
-            <li>
-              <Link to="/readme">readme</Link>
-            </li>
-          </ul>
-        </nav>
       </header>
       <Switch>
         /** * Main app route */
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+// Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles((theme) => ({
+  Nav: {
+    width: "100%",
+    background: theme.palette.primaryDark,
+    padding: "10px 0",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    "& ul": {
+      listStyle: "none",
+      padding: 0,
+      display: "flex",
+      justifyContent: "space-around",
+      width: "50%",
+    },
+    "& li": {
+      padding: "10px",
+    },
+    "& a": {
+      color: theme.palette.text,
+      textDecoration: "none",
+      fontSize: "1.2rem",
+      "&:hover": {
+        textDecoration: "underline",
+      },
+    },
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <nav className={classes.Nav}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/readme">Readme</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
